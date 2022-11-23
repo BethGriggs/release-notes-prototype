eda91209a37e119065e6e5294b5029585f0b937f
Beth Griggs <bethanyngriggs@gmail.com>
2022-11-23 13:16:22 +0000

    lint: add linter

--- fetchCommitsList.js
+++ fetchCommitsList.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
-import { parseArgs } from "node:util";
-import fs from "node:fs";
-import path from "node:path";
+import { parseArgs } from 'node:util';
+import fs from 'node:fs';
+import path from 'node:path';
 
 // parse CLI arguments
-export const options = {
+const options = {
   repository: {
-    type: "string",
+    type: 'string',
   },
   baseTag: {
-    type: "string",
+    type: 'string',
   },
   tag: {
-    type: "string",
+    type: 'string',
   },
   filename: {
-    type: "string",
+    type: 'string',
   },
 };
 
@@ -24,9 +24,9 @@ const {
   repository,
   baseTag,
   tag,
-  filename
+  filename,
 } = parseArgs({
-  options
+  options,
 }).values;
 
 const githubQuery = `https://api.github.com/repos/${repository}/compare/${baseTag}...${tag}`;
@@ -35,25 +35,23 @@ console.log(`Fetching commits from ${githubQuery}`);
 const githubResponse = await fetch(githubQuery);
 const githubResponseJson = await githubResponse.json();
 
-let JDK_ISSUES = [];
+const JDK_ISSUES = [];
+
+function isJDKIssue(line) {
+  return (/^[0-9]+:/).test(line);
+}
 
 for (const commit of githubResponseJson.commits) {
-
-  let commitLines = commit.commit.message.split("\n");
-  commitLines.forEach(line => {
+  const commitLines = commit.commit.message.split('\n');
+  commitLines.forEach((line) => {
     if (isJDKIssue(line)) {
       JDK_ISSUES.push({
-        "id": "JDK-" + line.split(":")[0],
-        "commit": commit.sha,
-        "title": line
+        id: `JDK-${line.split(':')[0]}`,
+        commit: commit.sha,
+        title: line,
       });
     }
   });
-}
-
-function isJDKIssue(line) {
-  const regexp = new RegExp(/^[0-9]+:/);
-  return regexp.test(line);
 }
 
 fs.writeFileSync(path.resolve(process.cwd(), `${filename}`), JSON.stringify(JDK_ISSUES, null, 2));
--- fetchReleaseNotes.js
+++ fetchReleaseNotes.js
@@ -3,75 +3,73 @@
 /* This script is used to fetch OpenJDK release notes from the bugs.openjdk.org */
 
 import {
-    parseArgs
-} from "node:util";
+  parseArgs,
+} from 'node:util';
 
-import fs from "node:fs";
-import path from "node:path";
+import fs from 'node:fs';
+import path from 'node:path';
 
-export const options = {
-    commitList: {
-        type: "string",
-    },
-    filename: {
-        type: "string",
-    },
-    version: {
-        type: "string",
-    }
+const options = {
+  commitList: {
+    type: 'string',
+  },
+  filename: {
+    type: 'string',
+  },
+  version: {
+    type: 'string',
+  },
 };
 
 const { commitList, filename, version } = parseArgs({ options }).values;
 
 const commits = fs.readFileSync(commitList);
 
-let output = [];
+const output = [];
 
 // fetch the release notes from the bugs.openjdk.org
-const baseUrl = "https://bugs.openjdk.java.net/rest/api/2/search?jql=";
-const jql = "project=JDK AND (status in (Closed, Resolved)) AND (resolution not in (\"Won't Fix\", \"Duplicate\", \"Cannot Reproduce\", \"Not an Issue\", \"Withdrawn\")) AND (labels not in (release-note, testbug, openjdk-na, testbug) OR labels is EMPTY) AND (summary !~ \"testbug\") AND (summary !~ \"problemlist\") AND (summary !~ \"problem list\") AND (summary !~ \"release note\") AND (issuetype != CSR) AND fixVersion=" + version;
+const baseUrl = 'https://bugs.openjdk.java.net/rest/api/2/search?jql=';
+const jql = `project=JDK AND (status in (Closed, Resolved)) AND (resolution not in ("Won't Fix", "Duplicate", "Cannot Reproduce", "Not an Issue", "Withdrawn")) AND (labels not in (release-note, testbug, openjdk-na, testbug) OR labels is EMPTY) AND (summary !~ "testbug") AND (summary !~ "problemlist") AND (summary !~ "problem list") AND (summary !~ "release note") AND (issuetype != CSR) AND fixVersion=${version}`;
 // execute the initial fetch to get the total number of issues
-const totalQuery = await fetch(baseUrl + jql + "&startAt=1&maxResults=1");
-const res = await totalQuery.json();
-const total = res.total;
+const totalQuery = await fetch(`${baseUrl + jql}&startAt=1&maxResults=1`);
+const initialRes = await totalQuery.json();
+const { total } = initialRes;
 
 // fetch all the issues by page
 for (let startAt = 0; startAt <= total + 50; startAt += 50) {
-    const query = await fetch(baseUrl + jql + "&startAt=" + startAt + "&maxResults=50");
-    const res = await query.json();
+  const query = await fetch(`${baseUrl + jql}&startAt=${startAt}&maxResults=50`);
+  const pageRes = await query.json();
 
-    res.issues.forEach(issue => {
+  pageRes.issues.forEach((issue) => {
+    let parent = '';
 
-        let parent = "";
+    if (issue.fields.issuetype.name === 'Backport') {
+      const linkedIssues = issue.fields.issuelinks;
 
-        if (issue.fields.issuetype.name === 'Backport') {
-
-            let linkedIssues = issue.fields.issuelinks;
-
-            linkedIssues.forEach(linkedIssue => {
-                if (linkedIssue.type.name === "Backport") {
-                    parent = linkedIssue.inwardIssue.key;
-                }
-            })
+      linkedIssues.forEach((linkedIssue) => {
+        if (linkedIssue.type.name === 'Backport') {
+          parent = linkedIssue.inwardIssue.key;
         }
+      });
+    }
 
-        // check if the issue or parent issue is in the commit list
-        if (commits.includes(issue.key) || commits.includes(parent)) {
-            console.log(issue.key + " is in the commit list");
-            output.push({
-                "id": issue.key,
-                "title": issue.fields.summary,
-                "priority": issue.fields.priority.id,
-                "component": issue.fields.components[0].name,
-                "subcomponent": `${issue.fields.components[0].name}${issue.fields.customfield_10008?.name ? '/' + issue.fields.customfield_10008?.name : ''}`,
-                "link": "https://bugs.openjdk.java.net/browse/" + issue.key,
-                "type": issue.fields.issuetype.name,
-                "backportOf": parent || null,
-            });
-        } else {
-            console.log(issue.key + " or " + parent + " is not in the commit list");
-        }
-    });
+    // check if the issue or parent issue is in the commit list
+    if (commits.includes(issue.key) || commits.includes(parent)) {
+      console.log(`${issue.key} is in the commit list`);
+      output.push({
+        id: issue.key,
+        title: issue.fields.summary,
+        priority: issue.fields.priority.id,
+        component: issue.fields.components[0].name,
+        subcomponent: `${issue.fields.components[0].name}${issue.fields.customfield_10008?.name ? `/${issue.fields.customfield_10008?.name}` : ''}`,
+        link: `https://bugs.openjdk.java.net/browse/${issue.key}`,
+        type: issue.fields.issuetype.name,
+        backportOf: parent || null,
+      });
+    } else {
+      console.log(`${issue.key} or ${parent} is not in the commit list`);
+    }
+  });
 }
 
 fs.writeFileSync(path.resolve(process.cwd(), `${filename}`), JSON.stringify(output, null, 2));
