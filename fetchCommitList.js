8f7188240d39b9bc1766f3c8b5648cf82dabff6d
Beth Griggs <bethanyngriggs@gmail.com>
2023-02-15 14:02:53 +0000

    script: add error handling to fetchCommitList

--- fetchCommitList.js
+++ fetchCommitList.js
@@ -19,6 +19,7 @@ const options = {
   },
   filename: {
     type: 'string',
+    default: 'jdk-commits.json',
   },
 };
 
@@ -31,6 +32,12 @@ const {
   options,
 }).values;
 
+// error if required arguments are missing
+if (!repository || !baseTag || !tag) {
+  console.error('Missing required arguments');
+  process.exit(1);
+}
+
 const commitsJson = await fetchCommits({
   repository,
   baseTag,
@@ -39,5 +46,9 @@ const commitsJson = await fetchCommits({
 console.log(`Fetched ${commitsJson.length} commits`);
 const JDK_ISSUES = await extractJDKIssues(commitsJson);
 
-console.log(`Writing JDK issues to ${filename}`);
-fs.writeFileSync(path.resolve(process.cwd(), `${filename}`), JSON.stringify(JDK_ISSUES, null, 2));
+try {
+  console.log(`Writing JDK issues to ${filename}`);
+  fs.writeFileSync(path.resolve(process.cwd(), `${filename}`), JSON.stringify(JDK_ISSUES, null, 2));
+} catch (error) {
+  console.error(`Error writing file ${filename}: ${error.message}`);
+}
